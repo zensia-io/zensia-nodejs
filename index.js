1d197eb3c980c61057ab2af6ad1558e0db29436b
hexck <hexkcc@gmail.com>
2022-09-30 11:54:47 +0200

    cache system

--- index.js
+++ index.js
@@ -1,48 +1,65 @@
 const axios = require('axios').default
+const NodeCache = require("node-cache")
+const md5 = require('md5')
+const sum = require('hash-sum')
 
 class Zensia {
-    constructor(apiKey) {
+    constructor(apiKey, cache = true) {
         this.apiKey = apiKey
+        this.caching = cache
+        this.cache = new NodeCache({
+            stdTTL: 100, checkperiod: 120
+        }
+        )
+    }
+
+    async handle(...props) {
+        try {
+            const key = sum(props)
+            if (this.caching && this.cache.has(key)) return this.cache.get(key)
+
+            const response = (await axios.post(...props, { headers: { 'x-zensia-api-key': this.apiKey } }).catch()).data?.data
+
+            if (this.caching && !this.cache.has(key)) this.cache.set(key, response)
+
+            return response
+        } catch (exc) {
+            // console.log(exc)
+            throw new Error("Something went wrong. Make sure that API key is correct.")
+        }
     }
 
     async translate(text, to, from = 'en') {
-        try {
-            return (await axios.post('https://api.zensia.io/translate/single', { from, to, text }, { headers: { 'x-zensia-api-key': this.apiKey } }).catch()).data?.data
-        } catch (exc) {
-            throw new Error("ZENSIA: Invalid API key")
-        }
+        return await this.handle('https://api.zensia.io/translate/single', { from, to, text })
     }
 
     async translateBulk(texts, to, from = 'en') {
-        try {
-            return (await axios.post('https://api.zensia.io/translate/bulk', { from, to, texts }, { headers: { 'x-zensia-api-key': this.apiKey } }).catch()).data?.data
-        } catch (exc) {
-            throw new Error("ZENSIA: Invalid API key")
-        }
+        return await this.handle('https://api.zensia.io/translate/bulk', { from, to, texts })
     }
 
     async translateHtml(html, to, from = 'en') {
-        try {
-            return (await axios.post('https://api.zensia.io/translate/html', { html, to, from }, { headers: { 'x-zensia-api-key': this.apiKey } }).catch()).data?.data
-        } catch (exc) {
-            throw new Error("ZENSIA: Invalid API key")
-        }
+        return await this.handle('https://api.zensia.io/translate/bulk', { from, to, html })
     }
 
     async translateHtmlBulk(htmls, to, from = 'en') {
-        try {
-            return (await axios.post('https://api.zensia.io/translate/html', { html: htmls, to, from }, { headers: { 'x-zensia-api-key': this.apiKey } }).catch()).data?.data
-        } catch (exc) {
-            throw new Error("ZENSIA: Invalid API key")
-        }
+        return await this.handle('https://api.zensia.io/translate/bulk', { from, to, html: htmls })
     }
 
     async translateCustom(payload, func) {
-        try {
-            return (await axios.post(`https://api.zensia.io/translate/${func}`, payload, { headers: { 'x-zensia-api-key': this.apiKey } }).catch()).data?.data
-        } catch (exc) {
-            throw new Error("ZENSIA: Invalid API key")
-        }
+        return await this.handle(`https://api.zensia.io/translate/${func}`, payload)
+        // try {
+        //     const key = md5(JSON.stringify(payload)+func)
+
+        //     if (this.caching && this.cache.has(key)) return this.cache.get(key)
+
+        //     const response = (await axios.post(`https://api.zensia.io/translate/${func}`, payload, { headers: { 'x-zensia-api-key': this.apiKey } }).catch()).data?.data
+
+        //     if (this.caching && !this.cache.has(key)) this.cache.set(key, response)
+
+        //     return response
+        // } catch (exc) {
+        //     throw new Error("Something went wrong. Make sure that API key is correct.")
+        // }
     }
 }
 
